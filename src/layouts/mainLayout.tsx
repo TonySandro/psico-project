c70c61d1b225b05e3b68bde1d1b130565988d5a7
Tony Sandro <tonysduarte101@gmail.com>
2025-06-16 14:39:54 -0300

    feat: adding navbar component

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SideBar from '../components/navigation/sidebar/sideBar';
+import NavBar from '../components/navigation/navbar/navBar';
 
 const MainLayout: React.FC = () => {
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', height: '100vh' }}>
       <SideBar />
-      <main style={{ flex: 1, padding: '16px' }}>
-        <Outlet />
-      </main>
+      <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+        <NavBar />
+        <div style={{ padding: '20px', flex: 1, overflowY: 'auto' }}>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
